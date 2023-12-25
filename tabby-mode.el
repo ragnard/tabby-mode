ed8d59d0c220eee5e0e35533fafbd124812a7a21
Ragnar Dahlén <r.dahlen@gmail.com>
2023-12-25 11:48:21 +0100

    Some improvements:
    - Support multiple, configurable languages based on major-mode
    - Make completion-function configurable
    - Docstrings

--- tabby-mode.el
+++ tabby-mode.el
@@ -17,18 +17,40 @@
   :type 'string
   :group 'tabby)
 
+(defcustom tabby-completion-function 'completing-read
+  "Function to use when selection a completion"
+  :type 'symbol
+  :group 'tabby)
+
+(defcustom tabby-mode-language-alist
+  '((c-mode . "c")
+    (c++-mode . "cpp")
+    (go-mode . "go")
+    (java-mode . "java")
+    (javascript-mode . "javascript")
+    (kotlin-mode . "kotlin")
+    (python-mode . "python")
+    (ruby-mode . "ruby")
+    (rust-mode . "rust")
+    (typescript-mode . "typescript")
+    (yaml-mode . "yaml"))
+  "Mapping from major mode to Tabby language identifier."
+  :type '(alist :key-type symbol :value-type string)
+  :group 'tabby)
+
 (defun tabby--completions-url ()
+  "Return the API url for completions."
   (format "%s/v1/completions" (string-remove-suffix "/" tabby-api-url)))
 
 (defun tabby--completions-request (lang prefix suffix)
-  "TODO."
+  "Build a completions request for LANG with PREFIX and SUFFIX."
   (json-encode
    `((language . ,lang)
      (segments . ((prefix . ,prefix)
                   (suffix . ,suffix))))))
 
 (defun tabby--get-completions (buffer lang prefix suffix callback)
-  "TODO."
+  "Send a completions request to the Tabby API."
   (let ((url-request-method "POST")
         (url-request-extra-headers `(("Content-Type" . "application/json")))
         (url-request-data (tabby--completions-request lang prefix suffix)))
@@ -39,14 +61,19 @@
                       (funcall callback buffer response))))))
 
 (defun tabby--handle-completion-response (buffer response)
-  "TODO."
+  "Handle a completions RESPONSE for a BUFFER."
   (let* ((choices (mapcar (lambda (c)
                             (alist-get 'text c))
                           (alist-get 'choices response)))
-         (text (completing-read "Tabby: " choices)))
+         (text (funcall tabby-completion-function "Tabby: " choices)))
     (when text
       (with-current-buffer buffer
         (insert text)))))
+
+(defun tabby--determine-language ()
+  "Determine the language identifier for the current buffer.
+See https://code.visualstudio.com/docs/languages/identifiers."
+  (alist-get major-mode tabby-mode-language-alist))
 
 (defun tabby-complete ()
   "Ask Tabby for completion suggestions on the text around point."
@@ -54,8 +81,10 @@
   (let ((prefix (buffer-substring (point-min) (point)))
         (suffix (when (< (point) (point-max))
                   (buffer-substring (+ (point) 1) (point-max))))
-        (lang "go"))
-    (tabby--get-completions (current-buffer) lang prefix suffix 'tabby--handle-completion-response)))
+        (lang (tabby--determine-language)))
+    (if lang
+        (tabby--get-completions (current-buffer) lang prefix suffix 'tabby--handle-completion-response)
+      (message "Unable to determine language for current buffer."))))
 
 (define-minor-mode tabby-mode
   "A mode for Tabby")
